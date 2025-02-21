9a7a4f6752c358d3393dc4aa3a36d9df93969838
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-02-21 15:20:43 -0500

    Update broadcaster_test.go

--- evm/heads/broadcaster_test.go
+++ evm/heads/broadcaster_test.go
@@ -60,7 +60,9 @@ func TestHeadBroadcaster_Subscribe(t *testing.T) {
 			chchHeaders <- chHead
 		}).
 		Return((<-chan *evmtypes.Head)(chHead), sub, nil)
-	ethClient.On("HeadByNumber", mock.Anything, mock.Anything).Return(testutils.Head(1), nil)
+
+	h1 := testutils.Head(1)
+	ethClient.On("HeadByNumber", mock.Anything, mock.Anything).Return(h1, nil)
 
 	sub.On("Unsubscribe").Return()
 	sub.On("Err").Return(nil)
@@ -82,18 +84,19 @@ func TestHeadBroadcaster_Subscribe(t *testing.T) {
 	assert.Equal(t, (*evmtypes.Head)(nil), latest1)
 
 	headers := <-chchHeaders
-	h := evmtypes.Head{Number: 1, Hash: utils.NewHash(), ParentHash: utils.NewHash(), EVMChainID: big.New(testutils.FixtureChainID)}
-	headers <- &h
+	headers <- h1
 	g.Eventually(checker1.OnNewLongestChainCount).Should(gomega.Equal(int32(1)))
 
 	latest2, _ := hb.Subscribe(checker2)
 	// "latest head" is set here to the most recent head received
 	assert.NotNil(t, latest2)
-	assert.Equal(t, h.Number, latest2.Number)
+	assert.Equal(t, h1.Number, latest2.Number)
 
 	unsubscribe1()
 
-	headers <- &evmtypes.Head{Number: 2, Hash: utils.NewHash(), ParentHash: h.Hash, EVMChainID: big.New(testutils.FixtureChainID)}
+	h2 := &evmtypes.Head{Number: 2, Hash: utils.NewHash(), ParentHash: h1.Hash, EVMChainID: big.New(testutils.FixtureChainID)}
+	h2.Parent.Store(h1)
+	headers <- h2
 	g.Eventually(checker2.OnNewLongestChainCount).Should(gomega.Equal(int32(1)))
 }
 
